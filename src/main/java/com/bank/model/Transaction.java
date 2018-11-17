72c77d6b97d189fb4c304ee2c70a2e5fe5ab95fe
malteserteresa <tess_the_titan@hotmail.com>
2018-11-17 11:15:13 +0100

    added defualt constructor for jackson

--- src/main/java/com/bank/model/Transaction.java
+++ src/main/java/com/bank/model/Transaction.java
@@ -7,18 +7,21 @@ import com.bank.util.Time;
 
 public class Transaction {
 
-	public final BigDecimal amount;
-	public final String timestamp;
-
-	public Transaction(BigDecimal amount, String timestamp) {
-		this.amount = amount;
-		this.timestamp = timestamp;
-
-	}
+	public BigDecimal amount;
+	public String timestamp;
 
 	public BigDecimal getAmount() {
 		// TODO NOT CORRECT BEHAVIOUR BUT WHAT IS?
 		return amount != null ? amount : new BigDecimal("0.00");
+	}
+
+	public Transaction() {
+
+	}
+
+	public Transaction(BigDecimal amount, String timestamp) {
+		this.amount = amount;
+		this.timestamp = timestamp;
 	}
 
 	public long getNanoseconds() throws DateTimeParseException {
